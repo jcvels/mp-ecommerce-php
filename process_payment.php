54b4d20a9076d49f7b28fb48c70de0852dedb4b5
Jorge Pauvels <jcvels@uvcoding.com.ar>
2020-06-11 00:40:44 -0300

    00.40

---
+++ process_payment.php
@@ -0,0 +1,18 @@
+<?php
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('TEST-7621668301728696-092619-35eac9b49acb2e6b6df9b2c9e7d75402-92016816');
+
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
+
+// Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->title = 'Mi producto';
+$item->quantity = 1;
+$item->unit_price = 75.56;
+$preference->items = array($item);
+$preference->save();
+?>
