b54a44e9ce8390d8d6cbfd96a2174aa6fe27cd06
Jorge Pauvels <jcvels@uvcoding.com.ar>
2020-06-11 14:59:12 -0300

    14.59

---
+++ pago_ipn.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = file_get_contents('php://input');
+$filename = "data-" . getdate()[0];
+
+if( $data ) { http_response_code(200); }
+else { http_response_code(400); }
+
+$file = fopen( "replies/" . $filename . ".txt", "a");
+fwrite($file, $data);
+fclose($file);
+
+?>
