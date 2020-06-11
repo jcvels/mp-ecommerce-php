beec161eb377e11f0875d2af67441bb6312787d9
Jorge Pauvels <jcvels@uvcoding.com.ar>
2020-06-11 01:16:57 -0300

    01.16

--- detail.php
+++ detail.php
@@ -131,14 +131,12 @@
                                         </h3>
                                     </div>
 
-                                    <form action="procesar-pago.php" method="POST">
+                                    <form action="/procesar-pago.php" method="POST">
                                         
                                         <script
                                             src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
                                             data-preference-id="<?php echo $preference->id; ?>">
                                         </script>
-                                    
-                                        <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                     
                                     </form>
                                     
